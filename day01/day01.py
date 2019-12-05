ee45df7943070b0c7443ac417e0ebc27cc28cd14
Aslak Skailand <aslakskailand@gmail.com>
2019-12-05 16:40:00 +0100

    forgot part two of day one :)

--- day01/day01.py
+++ day01/day01.py
@@ -9,15 +9,34 @@ def readFile():
 
 def getModuleFuel(modules):
     moduleMasses = 0
+    fuelFuelMass = 0
     for mass in modules:
         tempModuleMass = int(round(mass / 3,0)) - 2
         moduleMasses += tempModuleMass 
-    return moduleMasses
+        print(tempModuleMass)
+        fuelFuelMass += getFuelFuel(tempModuleMass)
+    return moduleMasses, fuelFuelMass
+
+def getFuelFuel(fuelMass):
+    tempFuel = fuelMass
+    cummulativeFuel = 0
+    while(tempFuel != 0): 
+        print(tempFuel)
+        tempFuel = int(round(tempFuel/3))-2
+        if(tempFuel < 0):
+            break
+        else:
+            cummulativeFuel += tempFuel
+       
+    return cummulativeFuel
+
+
 
 if __name__ == '__main__':
     print('Hohoho! Day 1!')
     modules = readFile()
     fuelNeeded = getModuleFuel(modules)
+    print(fuelNeeded[0] + fuelNeeded[1])
     print('Total amount of fuel needed: ' + str(fuelNeeded))
         
         
